89c9443cf3a66ebaf2fb557aaf69c913056e3912
neoeahit <neoeahit@gmail.com>
2016-03-25 15:18:05 -0400

    Fixes #4: allows hashbangs in the url

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,17 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.components';
+import {bootstrap} from 'angular2/platform/browser'
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {provide} from 'angular2/core';
+import 'rxjs/add/operator/map';
+import {AppComponent} from './app.components'
+import {LocationStrategy, Location, HashLocationStrategy } from 'angular2/router';
+
+import {RETService} from './services/RETService'
+
 
 import {ROUTER_PROVIDERS} from 'angular2/router';
 bootstrap(AppComponent, [
-  ROUTER_PROVIDERS
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS,
+  provide(LocationStrategy, {useClass: HashLocationStrategy}),
+  RETService
 ]);
